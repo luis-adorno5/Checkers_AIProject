dc9fae26e4c6670961403398627865e8da2d3db4
Jesus Moreno <moreno.jesus2212@gmail.com>
2021-04-26 22:16:44 -0400

    Implemented Visualization of Minimax Thinking Process (can be turned on or off) Easier difficulty selection for the game

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, WHITE
 from checkers.game import Game
 from checkers.minimax.algorithm import minimax
 
 FPS = 60
+difficulty = 2  # 1 = Easy, 2 = Normal, 3 = Advanced, 4 = Expert
+showThinking = True  # Setting this to True will show the game's thinking process
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
+
 
 def get_row_col_from_mouse(pos):
     x, y = pos
     row = y // SQUARE_SIZE
     col = x // SQUARE_SIZE
     return row, col
+
 
 def main():
     run = True
@@ -23,24 +27,25 @@ def main():
         clock.tick(FPS)
 
         if game.turn == WHITE:
-            value, new_board = minimax(game.get_board(), 3, WHITE, game)
+            value, new_board = minimax(game.get_board(), difficulty, WHITE, game, showThinking)
             game.ai_move(new_board)
 
-        if game.winner() != None:
+        if game.winner() is not None:
             print(game.winner())
             run = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
                 game.select(row, col)
 
         game.update()
-    
+
     pygame.quit()
+
 
 main()
